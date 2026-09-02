8ede2e03823c98350f5a3c61db58af6ef2e6177f
asaa9 <rendyekapras@gmail.com>
2026-09-03 00:30:04 +0800

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,54 +2,12 @@ apply(from = "repositories.gradle.kts")
 
 include(":core")
 
-// Load all modules under /lib
+// Load library
 File(rootDir, "lib").eachDir { include("lib:${it.name}") }
-
-// Load all modules under /lib-multisrc
 File(rootDir, "lib-multisrc").eachDir { include("lib-multisrc:${it.name}") }
 
-if (System.getenv("CI") != "true") {
-    // Local development (full project build)
-
-    /**
-     * Add or remove modules to load as needed for local development here.
-     */
-    loadAllIndividualExtensions()
-    // loadIndividualExtension("all", "jellyfin")
-} else {
-    // Running in CI (GitHub Actions)
-
-    val chunkSize = System.getenv("CI_CHUNK_SIZE").toInt()
-    val chunk = System.getenv("CI_CHUNK_NUM").toInt()
-
-    // Loads individual extensions
-    File(rootDir, "src").getChunk(chunk, chunkSize)?.forEach {
-        loadIndividualExtension(it.parentFile.name, it.name)
-    }
-}
-
-fun loadAllIndividualExtensions() {
-    File(rootDir, "src").eachDir { dir ->
-        dir.eachDir { subdir ->
-            loadIndividualExtension(dir.name, subdir.name)
-        }
-    }
-}
-fun loadIndividualExtension(lang: String, name: String) {
-    include("src:$lang:$name")
-}
-
-fun File.getChunk(chunk: Int, chunkSize: Int): List<File>? {
-    return listFiles()
-        // Lang folder
-        ?.filter { it.isDirectory }
-        // Extension subfolders
-        ?.mapNotNull { dir -> dir.listFiles()?.filter { it.isDirectory } }
-        ?.flatten()
-        ?.sortedBy { it.name }
-        ?.chunked(chunkSize)
-        ?.get(chunk)
-}
+// Daftarkan ekstensi Alqanime secara langsung
+include(":src:id:alqanime")
 
 fun File.eachDir(block: (File) -> Unit) {
     val files = listFiles() ?: return
@@ -59,5 +17,3 @@ fun File.eachDir(block: (File) -> Unit) {
         }
     }
 }
-include("src:id:alqanime")
-
